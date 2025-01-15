4fd0adaadb0fe22e6710c09e56c69860bc3c90f6
jelicm <jelicm2000@gmail.com>
2025-01-15 13:33:35 +0100

    done

--- lab12/minikube/run.sh
+++ lab12/minikube/run.sh
@@ -6,9 +6,6 @@ minikube start
 kubectl apply -f ./pv/main_pv.yaml
 kubectl apply -f ./pv/city_pv.yaml
 
-kubectl apply -f ./pv/main_pvc.yaml
-kubectl apply -f ./pv/city_pvc.yaml
-
 kubectl apply -f ./deployments/maindbdepl.yaml
 kubectl apply -f ./deployments/citydbdepl.yaml 
 kubectl apply -f ./deployments/maindepl.yaml
@@ -19,7 +16,5 @@ kubectl apply -f ./services/citydb_service.yaml
 kubectl apply -f ./services/main_service.yaml
 kubectl apply -f ./services/city_service.yaml
 
-
-echo "doneee"
 
 
